f1757f38e96124f95ce8d4a158331813f71208a4
jindac <dachanman91@gmail.com>
2017-01-16 16:12:09 -0800

    Create users table to accept username and password

--- app/models/user.js
+++ app/models/user.js
@@ -5,6 +5,16 @@ var Promise = require('bluebird');
 
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function() {
+    this.on('creating', function(model, attrs, options) {
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(model.get('password'), salt);
+
+      model.set('password', hash);
+      model.set('salt', salt);
+    });
+  }
 });
 
 module.exports = User;
